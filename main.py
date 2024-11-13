ba3b2a8a840e54a9e455d09fbb729813e35fa3fd
Anton <95656709+Anmati200@users.noreply.github.com>
2024-11-13 22:43:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#1st program
+print(9**0.5*5)
+
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+
+#4th program
+print(int(float('123.456') * 10 % 10))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
