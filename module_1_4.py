91273f1e62741b7d9345b7469cc36d8d3e5b30c8
Anton <95656709+Anmati22@users.noreply.github.com>
2024-11-20 20:47:38 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+# Организация программ и методы строк
+my_string = input('Введите название вашего любимого фильма или сериала: ')
+print('Количество символов, содержащихся в веденной строке: ', len(my_string))
+
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+
