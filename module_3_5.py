184ce357fb0ab2dd8c0aa0a1fb6cc5ca08e55ece
Anton <95656709+Anmati22@users.noreply.github.com>
2024-12-21 18:04:59 +0300

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -3,11 +3,13 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
+    if not first: #проверка, если в конце числа стоит ноль
+        first = 1
+        return first
     if len(str_number) == 1:
         return first
     else:
         return first * get_multiplied_digits(int(str_number[1:]))
 
-print(get_multiplied_digits(53094))
-print(get_multiplied_digits(9807600540321))
 print(get_multiplied_digits(12345))
+print(get_multiplied_digits(10200300400050000))
